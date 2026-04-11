11eb9c108b33c58210bd82196f09248eebf476aa
Steffen Meyer <myr.steffen@gmail.com>
2026-04-11 02:36:11 +0200

    feat(skeleton): add blog demo with components, filters, i18n, seeder, and DI-powered component factory

---
+++ lang/de/blog.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'title' => 'Blog',
+    'subtitle' => 'Neueste Beiträge aus der Preflow-Demo',
+    'read_more' => 'Weiterlesen',
+    'back' => 'Zurück zum Blog',
+    'no_posts' => 'Keine Beiträge gefunden.',
+    'filter' => 'Filter',
+    'all' => 'Alle',
+    'published' => 'Veröffentlicht',
+    'draft' => 'Entwurf',
+    'post_count' => '{0} Keine Beiträge|{1} Ein Beitrag|[2,*] :count Beiträge',
+];
---
+++ lang/en/blog.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'title' => 'Blog',
+    'subtitle' => 'Latest posts from the Preflow demo',
+    'read_more' => 'Read more',
+    'back' => 'Back to blog',
+    'no_posts' => 'No posts found.',
+    'filter' => 'Filter',
+    'all' => 'All',
+    'published' => 'Published',
+    'draft' => 'Draft',
+    'post_count' => '{0} No posts|{1} One post|[2,*] :count posts',
+];
