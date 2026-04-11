e23ffd844e4d4822d94a0ba5881d8b1c56e27376
Steffen Meyer <myr.steffen@gmail.com>
2026-04-11 12:45:38 +0200

    feat(skeleton): config reads from .env, debug uses integer levels

--- config/app.php
+++ config/app.php
@@ -1,9 +1,10 @@
 <?php
 
 return [
-    'name' => 'Preflow App',
-    'debug' => true,
-    'timezone' => 'UTC',
-    'locale' => 'en',
-    'key' => 'change-this-to-a-random-32-char-string!',
+    'name' => getenv('APP_NAME') ?: 'Preflow App',
+    // 0 = production, 1 = development, 2 = verbose (forces dev panels for all components)
+    'debug' => (int) (getenv('APP_DEBUG') ?: 0),
+    'timezone' => getenv('APP_TIMEZONE') ?: 'UTC',
+    'locale' => getenv('APP_LOCALE') ?: 'en',
+    'key' => getenv('APP_KEY') ?: '',
 ];
