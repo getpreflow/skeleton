5abb521bb7acd207be4a06e97af818680db72f10
Steffen Meyer <myr.steffen@gmail.com>
2026-04-11 22:11:20 +0200

    feat: Application supports MySQL driver and TypeRegistry, skeleton config updated

--- config/data.php
+++ config/data.php
@@ -10,6 +10,15 @@ return [
             'driver' => \Preflow\Data\Driver\JsonFileDriver::class,
             'path' => __DIR__ . '/../storage/data',
         ],
+        // 'mysql' => [
+        //     'driver' => \Preflow\Data\Driver\MysqlDriver::class,
+        //     'host' => getenv('DB_HOST') ?: '127.0.0.1',
+        //     'port' => getenv('DB_PORT') ?: '3306',
+        //     'database' => getenv('DB_NAME') ?: '',
+        //     'username' => getenv('DB_USER') ?: 'root',
+        //     'password' => getenv('DB_PASS') ?: '',
+        // ],
     ],
-    'default' => 'sqlite',
+    'default' => getenv('DB_DRIVER') ?: 'sqlite',
+    'models_path' => __DIR__ . '/models',
 ];
